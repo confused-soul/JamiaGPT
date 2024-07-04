bb5697b04fe78b34c2da59b467192b1a004196b8
Yasir <mdyasirkhan2003@gmail.com>
2024-07-04 21:35:56 +0530

    new vector database

--- make_vdb.py
+++ make_vdb.py
@@ -4,11 +4,11 @@ inference_api_key = huggingface_api
 from langchain_community.document_loaders import TextLoader
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceInferenceAPIEmbeddings
-from langchain_text_splitters import CharacterTextSplitter
+from langchain_text_splitters import CharacterTextSplitter, RecursiveCharacterTextSplitter
 
 loader = TextLoader("JMI.txt", encoding="utf-8")
 documents = loader.load()
-text_splitter = CharacterTextSplitter(separator="." ,chunk_size=400, chunk_overlap=150, length_function=len)
+text_splitter = RecursiveCharacterTextSplitter(separators=["\n\n", "\n", ".", " "], chunk_size=500, chunk_overlap=200, length_function=len)
 docs = text_splitter.split_documents(documents)
 embeddings = HuggingFaceInferenceAPIEmbeddings(
     api_key=inference_api_key, model_name="sentence-transformers/all-MiniLM-l6-v2"
@@ -16,4 +16,4 @@ embeddings = HuggingFaceInferenceAPIEmbeddings(
 db = FAISS.from_documents(docs, embeddings)
 print(db.index.ntotal)
 
-db.save_local("faiss_index")
+db.save_local("faiss_index2")
